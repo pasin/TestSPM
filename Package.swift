0458c2168a708a339b8ade19e4b2ccfa0218124b
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-03-12 11:08:48 -0700

    Test multiple targets

--- Package.swift
+++ Package.swift
@@ -9,21 +9,18 @@ let package = Package(
     products: [
         .library(
             name: "CouchbaseLiteSwift",
-            targets: ["CouchbaseLiteSwift"]),
-        .library(
-            name: "CouchbaseLiteSwift Community Edition",
-            targets: ["CouchbaseLiteSwift-CE"])
+            targets: ["CouchbaseLiteSwift-CE", "CouchbaseLiteSwift-EE"])
     ],
     targets: [
-        .binaryTarget(
-            name: "CouchbaseLiteSwift",
-            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.6/couchbase-lite-swift_xc_enterprise_3.1.6.zip",
-            checksum: "c5e870fa7ba491293bcf405d3da94b945d2c23aa63e683b4b61841435a9c332e"
-        ),
         .binaryTarget(
             name: "CouchbaseLiteSwift-CE",
             url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.0/couchbase-lite-swift_xc_community_3.1.0.zip",
             checksum: "556d6ae41df3b5ebf91dbe45d38214dd7032f97ac1b132f925356d20e3b9ada5"
+        ),
+        .binaryTarget(
+            name: "CouchbaseLiteSwift-EE",
+            url: "https://packages.couchbase.com/releases/couchbase-lite-ios/3.1.6/couchbase-lite-swift_xc_enterprise_3.1.6.zip",
+            checksum: "c5e870fa7ba491293bcf405d3da94b945d2c23aa63e683b4b61841435a9c332e"
         )
     ]
 )
